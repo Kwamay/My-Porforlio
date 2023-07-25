e2935fbdd63c7b0aac0d972bc470e1722d0a0216
kenneth adutwum <kwamayadu@gmail.com>
2023-07-25 09:10:42 +0000

    Fix eslint error

--- assets/js/desktop.js
+++ assets/js/desktop.js
@@ -1,81 +1,81 @@
 const projectDetails = [
-    {
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  {
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
       when an unknown printer took a galley of type and scrambled it 1960s.
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> <br/>
       Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer
       took a galley of type and scrambled it 1960s.Lorem Ipsum han printer took a galley of type
       and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-      featureImage: './images/dektop-images/nature.png',
-      technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
-      linkToLiveSection: 'https://kwamay.github.io/My-Porforlio/',
-      linkToSource: 'https://github.com/Kwamay/My-Porforlio',
-      name: 'Tonic',
-    },
-  
-    {
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it 1960s.
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> <br/>
-      Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer
-      took a galley of type and scrambled it 1960s.Lorem Ipsum han printer took a galley of type
-      and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-      featureImage: './images/dektop-images/art.png',
-      technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
-      linkToLiveSection: 'https://kwamay.github.io/My-Porforlio/',
-      linkToSource: 'https://github.com/Kwamay/My-Porforlio',
-      name: 'Multi-Post-Stories',
-    },
-  
-    {
-      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it 1960s.
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> <br/>
-      Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer
-      took a galley of type and scrambled it 1960s.Lorem Ipsum han printer took a galley of type
-      and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-      featureImage: './images/dektop-images/display-pic.png',
-      technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
-      linkToLiveSection: 'https://kwamay.github.io/My-Porforlio/',
-      linkToSource: 'https://github.com/Kwamay/My-Porforlio',
-      name: 'Facebook 360',
-    },
-  
-    {
-      description:`Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-      when an unknown printer took a galley of type and scrambled it 1960s.
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> <br/>
-      Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer
-      took a galley of type and scrambled it 1960s.Lorem Ipsum han printer took a galley of type
-      and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
-      featureImage: './images/dektop-images/avail.png',
-      technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
-      linkToLiveSection: 'https://kwamay.github.io/My-Porforlio/',
-      linkToSource: 'https://github.com/Kwamay/My-Porforlio',
-      name: 'Uber Navigation',
-    },
-  ];
+    featureImage: './images/dektop-images/nature.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    linkToLiveSection: 'https://kwamay.github.io/My-Porforlio/',
+    linkToSource: 'https://github.com/Kwamay/My-Porforlio',
+    name: 'Tonic',
+  },
 
-  // here this is a clickProjectListener for your desktop version.
+  {
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> <br/>
+      Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer
+      took a galley of type and scrambled it 1960s.Lorem Ipsum han printer took a galley of type
+      and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+    featureImage: './images/dektop-images/art.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    linkToLiveSection: 'https://kwamay.github.io/My-Porforlio/',
+    linkToSource: 'https://github.com/Kwamay/My-Porforlio',
+    name: 'Multi-Post-Stories',
+  },
+
+  {
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> <br/>
+      Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer
+      took a galley of type and scrambled it 1960s.Lorem Ipsum han printer took a galley of type
+      and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+    featureImage: './images/dektop-images/display-pic.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    linkToLiveSection: 'https://kwamay.github.io/My-Porforlio/',
+    linkToSource: 'https://github.com/Kwamay/My-Porforlio',
+    name: 'Facebook 360',
+  },
+
+  {
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+      when an unknown printer took a galley of type and scrambled it 1960s.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> <br/>
+      Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer
+      took a galley of type and scrambled it 1960s.Lorem Ipsum han printer took a galley of type
+      and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+    featureImage: './images/dektop-images/avail.png',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    linkToLiveSection: 'https://kwamay.github.io/My-Porforlio/',
+    linkToSource: 'https://github.com/Kwamay/My-Porforlio',
+    name: 'Uber Navigation',
+  },
+];
+
+// here this is a clickProjectListener for your desktop version.
 const clickProjectListener = (index) => {
-    // before show, set the modal details
-    const topic = document.querySelector('.pop-up-topic'); //
-    topic.innerHTML = projectDetails[index].name;
+  // before show, set the modal details
+  const topic = document.querySelector('.pop-up-topic'); //
+  topic.innerHTML = projectDetails[index].name;
 
-    const image = document.querySelector('.pop-up-img');
+  const image = document.querySelector('.pop-up-img');
   image.src = projectDetails[index].featureImage;
   const text = document.querySelector('.pop-up-text');
   text.innerHTML = projectDetails[index].description;
-   // see live
-   const seeLive = document.querySelector('.see-live');
-   seeLive.addEventListener('click', () => window.open(projectDetails[index].linkToLiveSection, '_blank'));
-   const seeSource = document.querySelector('.see-source');
-   seeSource.addEventListener('click', () => window.open(projectDetails[index].linkToSource, '_blank'));
-   // technologies
+  // see live
+  const seeLive = document.querySelector('.see-live');
+  seeLive.addEventListener('click', () => window.open(projectDetails[index].linkToLiveSection, '_blank'));
+  const seeSource = document.querySelector('.see-source');
+  seeSource.addEventListener('click', () => window.open(projectDetails[index].linkToSource, '_blank'));
+  // technologies
   const techParent = document.querySelector('.pop-tech');
   techParent.innerHTML = '';
   projectDetails[index].technologies.forEach((td) => {
